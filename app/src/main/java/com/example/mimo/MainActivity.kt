8ba14a03c24b795a25475b5b7acebeffe5efe372
민재로 <111555810+Minjaero@users.noreply.github.com>
2024-04-25 13:24:20 +0900

    OMG

--- app/src/main/java/com/example/mimo/MainActivity.kt
+++ app/src/main/java/com/example/mimo/MainActivity.kt
@@ -1,6 +1,10 @@
 package com.example.mimo
 
+import android.content.Intent
+import android.media.Image
 import android.os.Bundle
+import android.view.View.OnClickListener
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +19,20 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+
+            val markImageView: ImageView = findViewById(R.id.mark)
+
+            val homeImageView: ImageView = findViewById(R.id.home)
+
+            val homeImageView.setOnClickListener {
+                val intent = Intent(this, NextActivity::class.java)
+                startActivity(intent)
+            }
         }
-    }
+
+        }
+}
+
+class NextActivity {
+
 }
