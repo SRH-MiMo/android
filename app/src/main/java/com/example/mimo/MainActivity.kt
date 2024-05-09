54934f6d8665b932dabfd8c3dfb85189e434ca46
민재로 <111555810+Minjaero@users.noreply.github.com>
2024-05-09 12:11:36 +0900

    12345

--- app/src/main/java/com/example/mimo/MainActivity.kt
+++ app/src/main/java/com/example/mimo/MainActivity.kt
@@ -15,16 +15,16 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.textView)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-
+        
             }
         }
 
         }
-}
+
 
 class NextActivity {
 
